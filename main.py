36a554134e89c031e65b606de52fa8d194461e78
littlepadawan <ida.s.hellqvist@gmail.com>
2021-10-12 15:52:02 +0200

    Added function that empty and occupy a cell

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+test_list = [0,1,0,1,0,0]
+
 def occupied():
     # Represents an occupied cell
     return 1
@@ -13,3 +15,22 @@ def is_empty(cell_coordinates):
         return True
     else:
         return False
+
+def empty_cell(list_num, cell_num):
+    # Empties a cell
+    # list_num is the list to change
+    # cell_num is the list index to change
+    list_num[cell_num] = empty()
+    return list_num
+
+def occupy_cell(list_num, cell_num):
+    # Occupies a cell
+    # list_num is the list to change
+    # cell_num is the list index to change
+    list_num[cell_num] = occupied()
+    return list_num
+
+print(is_empty(test_list[1]))
+print(occupy_cell(test_list, 2))
+print(empty_cell(test_list, 2))
+    
