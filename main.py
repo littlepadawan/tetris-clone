00a8dd1feae20658cbd7a8d24e7b168bbd04e6f0
littlepadawan <ida.s.hellqvist@gmail.com>
2021-10-12 15:31:30 +0200

    Added function checking if a cell is empty

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ def occupied():
 def empty():
     # Represents an empty cell
     return 0
+
+def is_empty(cell_coordinates):
+    # Checks if a cell is empty
+    # cell_coordinates is a list index
+    if cell_coordinates == empty():
+        return True
+    else:
+        return False
