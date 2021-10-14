23bef75ec1dfe26d1d52cad0751f106275990736
littlepadawan <ida.s.hellqvist@gmail.com>
2021-10-14 16:05:39 +0200

    Added function moving a block left or right depending on direction

--- main.py
+++ main.py
@@ -28,9 +28,8 @@ def occupy_cell(list_index, cell_index):
     list_index[cell_index] = occupied()
     return list_index
 
-# access a cell test_grid[block['y']][block['x']]
-
 def move_down(block, grid):
+    # Move block one step down if cell under is empty
     try:
         next_y = grid[block['y']+1][block['x']]
         if is_empty(next_y) == True:
@@ -42,6 +41,7 @@ def move_down(block, grid):
         return(block, 'Hit ground')
 
 def move_left(block, grid):
+    # Move block one step left if cell to the left is empty
             next_x = grid[block['y']][block['x']-1]
             if block['x'] > 0 and is_empty(next_x) == True:
                 print('Its empty')
@@ -51,6 +51,7 @@ def move_left(block, grid):
                 return(block, 'Collision')
     
 def move_right(block, grid):
+    # Move block one step right if cell to the right is empty
         try:
             next_x = grid[block['y']][block['x']+1]
             if block['x'] < len(grid[block['y']]) and is_empty(next_x) == True:
@@ -61,6 +62,14 @@ def move_right(block, grid):
                 return(block, 'Collision')
         except IndexError:
             return(block, 'You are at the border')
+        
+def move_sideways(direction, block, grid):
+    # Move block sideways depending on input
+    if direction == 'left':
+        return move_left(block, grid)
+    if direction == 'right':
+        return move_right(block, grid)
+        
 
 test_grid = [[0,0,0,1,0],[0,0,0,1,0],[0,0,0,0,0]]
 test_list = [0,0,0,0,0,0]
@@ -74,4 +83,6 @@ print(move_down(test_block, test_grid))
 print(move_left(test_block, test_grid))
 print(move_right(test_block, test_grid)) 
 
-    
+#move_sideways('left', test_block, test_grid)
+
+
