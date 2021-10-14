a7deee217dc356c549dbe4ff29d1e69cd9125f76
littlepadawan <ida.s.hellqvist@gmail.com>
2021-10-14 14:51:09 +0200

    Added function moving a block one step right

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-test_grid = [[0,0,0,1,0],[0,0,0,1,0],[0,0,0,0,0]]
-test_list = [0,0,0,0,0,0]
-
 def occupied():
     # Represents an occupied cell
     return 1
@@ -31,11 +28,6 @@ def occupy_cell(list_index, cell_index):
     list_index[cell_index] = occupied()
     return list_index
 
-print(is_empty(test_list[1]))
-print(occupy_cell(test_list, 2))
-print(empty_cell(test_list, 2))
-
-
 # access a cell test_grid[block['y']][block['x']]
 
 def move_down(block, grid):
@@ -50,7 +42,6 @@ def move_down(block, grid):
         return(block, 'Hit ground')
 
 def move_left(block, grid):
-        try:
             next_x = grid[block['y']][block['x']-1]
             if block['x'] > 0 and is_empty(next_x) == True:
                 print('Its empty')
@@ -58,9 +49,29 @@ def move_left(block, grid):
                 return(block, 'Block x-coordinate updated!')
             else:
                 return(block, 'Collision')
+    
+def move_right(block, grid):
+        try:
+            next_x = grid[block['y']][block['x']+1]
+            if block['x'] < len(grid[block['y']]) and is_empty(next_x) == True:
+                print('Its empty')
+                block['x'] = block['x']+1
+                return(block, 'Block x-coordinate updated!')
+            else:
+                return(block, 'Collision')
         except IndexError:
-            return(block, 'You are at a border')
-    
-    
+            return(block, 'You are at the border')
+
+test_grid = [[0,0,0,1,0],[0,0,0,1,0],[0,0,0,0,0]]
+test_list = [0,0,0,0,0,0]
+test_block = {'x': 2, 'y':0}
+
+#print(is_empty(test_list[1]))
+#print(occupy_cell(test_list, 2))
+#print(empty_cell(test_list, 2))            
+
+print(move_down(test_block, test_grid))
+print(move_left(test_block, test_grid))
+print(move_right(test_block, test_grid)) 
 
     
